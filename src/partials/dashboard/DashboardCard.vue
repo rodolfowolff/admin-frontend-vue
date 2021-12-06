71eb45aea584665849ea51d7e9442bbadc3fca1b
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-06 16:57:02 -0300

    fix: add font opens sans, adjuste distance margin

--- src/partials/dashboard/DashboardCard.vue
+++ src/partials/dashboard/DashboardCard.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="col-span-full xl:col-span-8 bg-white shadow-lg rounded-sm border border-gray-200">
-    <header class="flex items-center px-5 py-4">
+    <header class="flex items-center px-4 py-2">
       <button class="h-10 w-28 rounded-md text-sm font-semibold
           text-white bg-at-prymary shadow-md">
         <span>Cadastrar</span>
       </button>
       
       <div :id="id" class="flex justify-end min-w-0 flex-1 md:px-8 lg:px-0">
-        <div class="flex items-center px-6 py-3 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
+        <div class="flex items-center pl-6 py-3 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
           <div class="w-44">
-            <label for="search" class="sr-only">Buscar</label>
+            <label for="search" class="sr-only">Busca</label>
             <div class="relative">
               <div class="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
                 <SearchIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -18,7 +18,7 @@
                 id="search" 
                 name="search" 
                 class="bg-gray-50 block w-full border border-t border-at-border rounded-md py-2 pl-10 pr-3 text-sm 
-                placeholder-gray-500 focus:outline-none focus:text-gray-500 focus:placeholder-gray-400 
+                placeholder-at-gray33 focus:outline-none focus:text-gray-500 focus:placeholder-gray-400 
                 focus:ring-1 focus:ring-rose-500 focus:border-rose-500 sm:text-sm" 
                 placeholder="Buscar" 
                 type="search" />
@@ -28,25 +28,25 @@
       </div>
     </header>
 
-    <div class="-my-2 overflow-x-auto">
-      <div class="pb-10 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+    <div class="overflow-x-auto">
+      <div class="pb-10 align-middle inline-block min-w-full sm:px-4">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
           <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
               <tr>
-                <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-at-gray54 tracking-wider">
+                <th scope="col" class="px-6 py-3 font-display text-left text-sm font-medium text-at-gray54 tracking-wider">
                   ID
                 </th>
-                <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-at-gray54 tracking-wider">
+                <th scope="col" class="px-6 py-3 font-display text-left text-sm font-medium text-at-gray54 tracking-wider">
                   Nome
                 </th>
-                <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-at-gray54 tracking-wider">
+                <th scope="col" class="px-6 py-3 font-display text-left text-sm font-medium text-at-gray54 tracking-wider">
                   Idade
                 </th>
-                <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-at-gray54 tracking-wider">
+                <th scope="col" class="px-6 py-3 font-display text-left text-sm font-medium text-at-gray54 tracking-wider">
                   GitHub User
                 </th>
-                <th scope="col" class="px-6 py-3 text-left text-sm font-semibold text-at-gray54 tracking-wider">
+                <th scope="col" class="px-6 py-3 font-display text-left text-sm font-medium text-at-gray54 tracking-wider">
                   Endereço
                 </th>
                 <th scope="col" class="relative px-6 py-3">
@@ -57,28 +57,29 @@
                 </th>
               </tr>
             </thead>
+
             <tbody class="bg-white divide-y divide-gray-200">
-              <tr v-for="person in people" :key="person.email" class="hover:bg-at-bgtrhover">
-                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-at-gray87">
-                  {{ person.id }}
+              <tr v-for="user in users" :key="user.username" class="hover:bg-at-bgtrhover">
+                <td class="px-6 py-4 font-display whitespace-nowrap text-sm text-at-gray87">
+                  {{ user.cod }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-at-gray87">
-                  {{ person.name }}
+                <td class="px-6 py-4 font-display whitespace-nowrap text-sm text-at-gray87">
+                  {{ user.username }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-at-gray87">
-                  {{ person.age }}
+                <td class="px-6 py-4 font-display whitespace-nowrap text-sm text-at-gray87">
+                  {{ user.dateofbirth }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-at-gray87">
-                  {{ person.github_user }}
+                <td class="px-6 py-4 font-display whitespace-nowrap text-sm text-at-gray87">
+                  {{ user.githubusername }}
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap text-sm text-at-gray87">
-                  {{ person.address }}
+                <td class="px-6 py-4 font-display whitespace-nowrap text-sm text-at-gray87">
+                  {{ user.address }}
                 </td>
-                <td class="px-2 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Ver</a>
+                <td class="px-2 py-4 font-display whitespace-nowrap text-right text-sm">
+                  <Eye />
                 </td>
-                <td class="px-3 py-4 whitespace-nowrap text-right text-sm font-medium">
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Deletar</a>
+                <td class="px-3 py-4 font-display whitespace-nowrap text-right text-sm">
+                  <Trash />
                 </td>
               </tr>
             </tbody>
@@ -91,23 +92,37 @@
 </template>
 
 <script>
+import api from "../../api";
 import { SearchIcon } from '@heroicons/vue/solid'
-const people = [
-  { id: '123456', name: 'João da Sila', age: '18 anos', github_user: 'joaodev', address: 'rua x do Brasil, 889 - Bairro teste - Curitiba/PR' },
-  { id: '123456', name: 'João da Sila', age: '18 anos', github_user: 'joaodev', address: 'rua x do Brasil, 889 - Bairro teste - Curitiba/PR' },
-  { id: '123456', name: 'João da Sila', age: '18 anos', github_user: 'joaodev', address: 'rua x do Brasil, 889 - Bairro teste - Curitiba/PR' },
-  { id: '123456', name: 'João da Sila', age: '18 anos', github_user: 'joaodev', address: 'rua x do Brasil, 889 - Bairro teste - Curitiba/PR' },
-]
+import Trash from '../../components/icons/Trash.vue'
+import Eye from '../../components/icons/Eye.vue'
 
 export default {
   name: 'Dashboard',
     components: {
-    SearchIcon
+    SearchIcon,
+    Trash,
+    Eye
   },
-  setup() {
-    return {
-      people,
-    }
-  },
-}
+  data() {
+     return {
+       users: {},
+     };
+   },
+   created() {
+     this.getUser();
+   },
+   methods: {
+     getUser() {
+       api
+         .get("/users")
+         .then((res) => {
+           this.users = res.data;
+         })
+         .catch((error) => {
+           console.log(error);
+         });
+     },
+   },
+  }
 </script>
