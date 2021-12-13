d6975e5e392e64f6b7094350fa593ab0fc400b01
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-13 11:46:10 -0300

    feat: store actions, mutations and getters

--- src/store.js
+++ src/store.js
@@ -1,13 +1,43 @@
 import { createStore } from 'vuex';
-import mutations from './store/mutations';
-import actions from './store/actions';
-import getters from './store/getters';
+import UserService from './api/user';
+// import mutations from './store/mutations';
+// import actions from './store/actions';
+// import getters from './store/getters';
 
 export default createStore({
   state: {
     users: [],
   },
-  mutations,
-  actions,
-  getters,
+  mutations: {
+    createUser(state, user) {
+      state.users.push(user);
+    },
+    setUsers(state, users) {
+      state.users = users;
+    },
+    deleteUser(state, cod) {
+      const userCod = state.users.find(user => user.cod == cod);
+      state.users.splice(state.users.indexOf(userCod), 1);
+    },
+  },
+  actions: {
+    createUser({ commit }, user) {
+      UserService.createUser(user).then(response => {
+        commit('createUser', response);
+      });
+    },
+    setUsers({ commit }, users) {
+      commit('setUsers', users);
+    },
+    deleteUser({ commit }, cod) {
+      UserService.deleteUser(cod).then(() => {
+        commit('deleteUser', cod);
+      });
+    }
+  },
+  getters: {
+    users(state) {
+      return state.users;
+    }
+  },
 });
