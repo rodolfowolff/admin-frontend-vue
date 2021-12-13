ca0c1150cf945f30d1f78fb68b52ab8e2ccb9491
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-13 11:48:39 -0300

    feat: comment code not using in that moment

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,17 +1,17 @@
-import UserService from '@/api/user';
+// import UserService from '@/api/user';
 
-export default {
-  createUser({ commit }, user) {
-    UserService.createUser(user).then(response => {
-      commit('createUser', response);
-    });
-  },
-  setUsers({ commit }, users) {
-    commit('setUsers', users);
-  },
-  deleteUser({ commit }, cod) {
-    UserService.deleteUser(cod).then(() => {
-      commit('deleteUser', cod);
-    });
-  }
-};
+// export default {
+//   createUser({ commit }, user) {
+//     UserService.createUser(user).then(response => {
+//       commit('createUser', response);
+//     });
+//   },
+//   setUsers({ commit }, users) {
+//     commit('setUsers', users);
+//   },
+//   deleteUser({ commit }, cod) {
+//     UserService.deleteUser(cod).then(() => {
+//       commit('deleteUser', cod);
+//     });
+//   }
+// };
