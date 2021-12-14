9d2ec32bc38730e2bf0d15f00395e725e6dee076
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-13 23:59:28 -0300

    feat: remove focus button close modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -27,8 +27,7 @@
             <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
               <button 
                 type="button" 
-                class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none 
-                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" 
+                class="bg-white rounded-md text-gray-400 hover:text-gray-500" 
                  @click="onReset">
                 <span class="sr-only">Close</span>
                 <XIcon class="h-6 w-6" aria-hidden="true" />
--- src/components/ModalDelete.vue
+++ src/components/ModalDelete.vue
@@ -30,11 +30,10 @@
             <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
               <button 
                 type="button"
-                class="bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none 
-                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" 
-                @click="onReset">
+                class="bg-white rounded-md text-gray-400 hover:text-gray-500" @click="onReset">
                 <span class="sr-only">Close</span>
-                <XIcon class="h-5 w-5 bg-at-border text-at-gray54 rounded-full hover:bg-at-gray33 hover:text-at-gray87" aria-hidden="true" />
+                <XIcon class="h-5 w-5 bg-at-border text-at-gray54 rounded-full hover:bg-at-gray33 hover:text-at-gray87" 
+                  aria-hidden="true" />
               </button>
             </div>
             <div class="sm:flex sm:items-start">
