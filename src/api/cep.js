322be73be42eb7a2c8910bd8502883ea89a41b59
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-13 21:01:22 -0300

    feat: add console log error in cep return

--- src/api/cep.js
+++ src/api/cep.js
@@ -1,4 +1,4 @@
-export async function getCEPByAddress(address) {
+export default async function getCEPByAddress(address) {
   try {
     const response = await fetch(`https://viacep.com.br/ws/${address}/json/`);
     const data = await response.json();
